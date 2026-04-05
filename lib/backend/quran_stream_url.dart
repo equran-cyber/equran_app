542c4b108cc5b385533a5704f6ba206543d81228
ya27hw <yousuf5550@gmail.com>
2026-04-06 03:13:26 +0400

    remove reciters

--- lib/backend/quran_stream_url.dart
+++ lib/backend/quran_stream_url.dart
@@ -3,13 +3,13 @@ import 'package:equran/utils/library.dart' show AppReciter;
 
 class QuranAudioService {
   String getSurahUrl(int surah) {
-    final savedCode = SettingsDB().get('reciter', defaultValue: "ar.yasseraldossari");
+    final savedCode = SettingsDB().get('reciter', defaultValue: "ar.alafasi");
     final reciter = AppReciter.fromCode(savedCode);
 
     return reciter.surahUrl(surah);
   }
   String getAyahUrl(int surah, int ayah) {
-    final savedCode = SettingsDB().get("reciter", defaultValue: "ar.yasseraldossari");
+    final savedCode = SettingsDB().get("reciter", defaultValue: "ar.alafasi");
     final reciter = AppReciter.fromCode(savedCode);
 
     return reciter.ayahUrl(surah, ayah);
