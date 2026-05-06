a7e011b5bf6b3f2969e7d0f3ad479bc92cb0d02c
equran-cyber <equran-cyber@users.noreply.github.com>
2026-05-06 00:21:19 +0000

    deploy: 65782db06942afbe08281cbf9ad19339491c96b8

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "837406065" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3687984882" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
